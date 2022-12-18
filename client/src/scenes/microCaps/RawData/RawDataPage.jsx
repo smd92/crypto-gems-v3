0ef4cd225c46a1a43a5b4af433557d5579a1444c
smd92 <69214590+smd92@users.noreply.github.com>
2022-12-18 12:28:58 +0100

    .

---
+++ client/src/scenes/microCaps/RawData/RawDataPage.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import RawDataTable from "./RawDataTable";
+
+const RawDataPage = () => {
+  return <RawDataTable />;
+};
+
+export default RawDataPage;
---
+++ client/src/scenes/microCaps/RawData/RawDataTable.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { useSelector } from "react-redux";
+import DataTable from "components/DataTable";
+
+const RawDataTable = () => {
+  return (
+    <div>RawDataTable</div>
+  )
+}
+
+export default RawDataTable
